6e223fe6aed1808958657b9392ee6f7e6a6eeb65
emekanwaoma <chukwuemeka.nwaoma@port.io>
2025-09-01 00:02:19 +0100

    fix: add aws key

--- vuln_py_13.py
+++ vuln_py_13.py
@@ -1,5 +1,7 @@
 
 import subprocess
 GIHUB_TOKEN="ghp_j8VVpAkn252kKeXbZrDzt1lgPMPhy72CCah1"
+AWS_SECRET_ACCESS_KEY="wJalrXUtnFEMI/K7MDENG/bPxRfiCYFAKEKEY1234"
+
 def cmd_13():
     subprocess.call("ls", shell=True)
