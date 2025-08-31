d3948eccefe8b0bf0c9357cae01854fc0ab510c5
emekanwaoma <chukwuemeka.nwaoma@port.io>
2025-08-31 23:59:44 +0100

    fix: add tokens

--- vuln_py_13.py
+++ vuln_py_13.py
@@ -1,5 +1,5 @@
 
 import subprocess
-GIHUB_TOKEN="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"
+GIHUB_TOKEN="ghp_j8VVpAkn252kKeXbZrDzt1lgPMPhy72CCah1"
 def cmd_13():
     subprocess.call("ls", shell=True)
